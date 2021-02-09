d8b231da4cfee13b03c66edd6e240661f827f224
Vladimir <freshcode.yt@gmail.com>
2021-02-09 19:56:36 +0200

    Add react render example

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-'use strict';
+/*
+React;
+ReactDOM;
+ */
+
+const reactElement = React.createElement(
+  'h1',
+  { title: 'Hello react', className: 'heading' },
+  'Hello React.js'
+);
+
+ReactDOM.render(reactElement, document.getElementById('root'));
